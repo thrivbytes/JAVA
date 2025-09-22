a30a6c7077a35416823fdd45a1810cc3684ded08
Thrivikram Mekala <thrivikram642006@gmail.com>
2025-09-22 11:05:06 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class first{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers : ");
+        int a = sc.nextInt();
+        if(a > 0) {
+            System.out.println("The number is positive");
+        }
+        else{
+            System.out.println("The number is negative");
+        }
+
+
+        
+    }
+}
